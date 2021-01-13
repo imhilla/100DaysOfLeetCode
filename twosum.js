a6d9439047d9f53d205faf16264532ff8f365822
imhilla <hillaryodhiambo282@gmail.com>
2021-01-13 12:48:18 +0300

    add two pass solution

--- twosum.js
+++ twosum.js
@@ -30,6 +30,20 @@ const object1 = {
 console.log(Object.keys(object1));
 // expected output: Array ["a", "b", "c"]
 
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find
+
+// The find() method returns the value of the first element in the 
+// provided array that satisfies the provided testing function.If no values 
+// satisfies the testing function, undefined is returned.
+
+const array1 = [5, 12, 8, 130, 44];
+
+const found = array1.find(element => element > 10);
+
+console.log(found);
+// expected output: 12
+
+
 var twoSum = function (nums, target) {
   var hash = {};
   arr = [];
@@ -39,11 +53,9 @@ var twoSum = function (nums, target) {
 
   for (var i = 0; i < nums.length; i++) {
     remainder = target - nums[i]
-    if (Object.keys(hash).find(key => hash[key] === remainder)) {
-      if (Object.keys(hash).find(key => key !== i)) {
-        arr.push(i)
-        console.log(i)
-      }
+    if (Object.keys(hash).find(key => hash[key] === remainder && key !== i )) {
+      //   arr.push(i)
+      console.log(i)
     }
   }
   return arr
